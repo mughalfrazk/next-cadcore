b2352524d71e7974fbb25182dd17793172b1b5bc
Faraz Khalil <mughalfrazk@gmail.com>
2024-08-03 13:08:55 +0100

    initial commit

---
+++ src/components/layout/appShell/index.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import { AppShell, Burger, Group, Skeleton, Title } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import React, { ReactNode } from "react";
+
+const MainLayout = ({ children }: { children: ReactNode }) => {
+  const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
+  const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger
+            opened={mobileOpened}
+            onClick={toggleMobile}
+            hiddenFrom="sm"
+            size="sm"
+          />
+          <Burger
+            opened={desktopOpened}
+            onClick={toggleDesktop}
+            visibleFrom="sm"
+            size="sm"
+          />
+          <Title order={2}>Logo</Title>
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
+  );
+};
+
+export default MainLayout;
